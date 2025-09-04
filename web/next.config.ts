271c53be1ebc621c0a0865a4ec59a4d39d6824f8
aleksmontanha <aleksmontanha@gmail.com>
2025-09-04 15:52:28 -0300

    fix(web): update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // liste TODAS as origens que vão abrir o seu app em DEV
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://192.168.13.210:3000", // IP do servidor que roda o Next
-    "http://192.168.13.120:3000"  // máquina cliente que acessa
-  ],
+  // permita abrir o app a partir desses “origins” na rede local
+  allowedDevOrigins: ['192.168.13.120', '*.local', 'localhost'],
 };
 
-// Para Next com TypeScript, exporte como default
-export default nextConfig;
+module.exports = nextConfig;
